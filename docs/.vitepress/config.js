d26b189bcc91d2b03d9b8ffcbcb92586ff6c6200
skirtle <65301168+skirtles-code@users.noreply.github.com>
2021-10-03 15:30:34 +0100

    Flatten the components sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -17,30 +17,20 @@ module.exports = {
           text: 'Example Components',
           children: [
             {
-              text: 'Form Components',
-              children: [
-                {
-                  text: 'Checkbox',
-                  link: '/components/checkbox.html',
-                }, {
-                  text: 'Radio',
-                  link: '/components/radio.html',
-                }, {
-                  text: 'Toggle Switch',
-                  link: '/components/toggle-switch.html',
-                }
-              ]
+              text: 'Checkbox',
+              link: '/components/checkbox.html',
             }, {
-              text: 'Container Components',
-              children: [
-                {
-                  text: 'Accordion',
-                  link: '/components/accordion.html',
-                }, {
-                  text: 'Tabs',
-                  link: '/components/tabs.html',
-                }
-              ]
+              text: 'Radio',
+              link: '/components/radio.html',
+            }, {
+              text: 'Toggle Switch',
+              link: '/components/toggle-switch.html',
+            }, {
+              text: 'Accordion',
+              link: '/components/accordion.html',
+            }, {
+              text: 'Tabs',
+              link: '/components/tabs.html',
             }
           ]
         }, {
