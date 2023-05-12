12fce5e366197bcf60c14f5deec39bf153a1958e
skirtle <65301168+skirtles-code@users.noreply.github.com>
2023-05-12 04:25:13 +0100

    Remove unnecessary CSS from tab.vue

--- docs/components/tabs/tab.vue
+++ docs/components/tabs/tab.vue
@@ -24,33 +24,3 @@ const { active, unregister } = register(tab)
 
 onUnmounted(unregister)
 </script>
-
-<style scoped>
-.accordion-panel {
-  display: flex;
-  flex-direction: column;
-}
-
-.accordion-panel + .accordion-panel {
-  margin-top: 1px;
-}
-
-.header {
-  background-color: #e6f6ff;
-  border: 1px solid #ccc;
-  cursor: pointer;
-  margin: -1px;
-  padding: 5px;
-}
-
-.body {
-  background-color: #f7fcff;
-  border: 1px solid #ccc;
-  flex: auto;
-  margin: 0 -1px -1px;
-}
-
-.expanded {
-  flex: auto;
-}
-</style>
